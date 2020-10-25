20e284fdb91a1ebb75f49d4c7a67962580461c9f
wazirafghan <wazirhashmi@yahoo.com>
2020-10-25 22:01:51 +0500

    create migration

--- database/migrations/2020_10_25_162054_create_reviews_table.php
+++ database/migrations/2020_10_25_162054_create_reviews_table.php
@@ -15,6 +15,11 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->text('reviw');
+            $table->integer('star');
             $table->timestamps();
         });
     }
